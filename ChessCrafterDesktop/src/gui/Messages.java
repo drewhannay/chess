4ae9c65b7540e5838f6d16dea5a980a799af3d3f
Drew Hannay <drewhannay@gmail.com>
2013-10-13 01:16:50 -0500

    Issue #105: Putting all .properties files in a "resources" src directory and giving them better names
    This is required to get the project building with gradle

--- ChessCrafterDesktop/src/gui/Messages.java
+++ ChessCrafterDesktop/src/gui/Messages.java
@@ -1,13 +1,14 @@
 package gui;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages
 {
-	private static final String BUNDLE_NAME = "gui.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "GuiStrings"; //$NON-NLS-1$
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.US);
 
 	private Messages()
 	{
