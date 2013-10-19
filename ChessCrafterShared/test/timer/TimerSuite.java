084fb52c1c2d0df2e379d1204c4b48066277b042
Drew Hannay <drewhannay@gmail.com>
2013-10-18 21:22:27 -0500

    Issue #82 Add dummy unit tests and support running them in the gradle build scripts

---
+++ ChessCrafterShared/test/timer/TimerSuite.java
@@ -0,0 +1,12 @@
+package timer;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ChessTimerTest.class })
+public class TimerSuite
+{
+
+}
