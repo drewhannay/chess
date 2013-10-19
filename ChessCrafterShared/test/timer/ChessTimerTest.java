8e7ce86ff0fce6ba78a2555afadbc679653abad6
Drew Hannay <drewhannay@gmail.com>
2013-10-18 21:31:16 -0500

    Issue #82 Commenting out the failing dummy unit tests so builds won't fail

--- ChessCrafterShared/test/timer/ChessTimerTest.java
+++ ChessCrafterShared/test/timer/ChessTimerTest.java
@@ -1,7 +1,5 @@
 package timer;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -33,108 +31,108 @@ public class ChessTimerTest
 	@Test
 	public final void testInit()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testActionPerformed()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testIsNoTimer()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testIsWordTimer()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testCreateTimer()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testStopTimers()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testGetClockDirection()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testGetDisplayLabel()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testGetStartTime()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testGetRawTime()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testReset()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testRestart()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testSetClockDirection()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testSetClockTime()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testStartTimer()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testStopTimer()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testTimeElapsed()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public final void testUpdateDisplay()
 	{
-		fail("Not yet implemented"); // TODO
+		// fail("Not yet implemented"); // TODO
 	}
 }
