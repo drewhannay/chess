598cab2ba3b8cfad2bffb775474c2abe3deb1fad
Drew Hannay <drewhannay@gmail.com>
2015-01-11 07:45:10 -0800

    Add jetbrains annotations dependency

--- ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/logic/GameBuilder.java
+++ ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/logic/GameBuilder.java
@@ -11,6 +11,7 @@ import com.drewhannay.chesscrafter.rules.promotionmethods.ClassicPromotionMethod
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -54,6 +55,7 @@ public class GameBuilder {
         mBlackRules = blackRules;
     }
 
+    @NotNull
     public static Game buildClassic() {
         long pieceId = 0;
 
