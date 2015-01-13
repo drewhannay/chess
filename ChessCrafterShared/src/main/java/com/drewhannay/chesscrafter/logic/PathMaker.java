de95516e64d0703ebb6bb2199ced4d018f968eb6
jmccormi <john.mccormick@my.wheaton.edu>
2015-01-13 12:31:07 -0600

    Fixing broken PathMaker

--- ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/logic/PathMaker.java
+++ ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/logic/PathMaker.java
@@ -31,7 +31,7 @@ public class PathMaker {
         int least = Math.min(mOrigin.x, mDestination.x);
         int most = Math.max(mOrigin.x, mDestination.x);
 
-        List<ChessCoordinate> spaces = new ArrayList();
+        List<ChessCoordinate> spaces = new ArrayList<>();
         for (int x = least + 1, steps = 1; x <= most && steps <= maxSteps; x++, steps++) {
             if (Math.abs(mOrigin.x - x) <= maxSteps) {
             spaces.add(ChessCoordinate.at(x, mOrigin.y));
@@ -46,7 +46,7 @@ public class PathMaker {
         int least = Math.min(mOrigin.y, mDestination.y);
         int most = Math.max(mOrigin.y, mDestination.y);
 
-        List<ChessCoordinate> spaces = new ArrayList();
+        List<ChessCoordinate> spaces = new ArrayList<>();
         for (int y = least + 1, steps = 1; y <= most && steps <= maxSteps; y++, steps++) {
             if (Math.abs(mOrigin.y - y) <= maxSteps) {
             spaces.add(ChessCoordinate.at(mOrigin.x, y));
@@ -62,7 +62,7 @@ public class PathMaker {
         int xDirection = (mDestination.x - mOrigin.x) / absoluteDistance;
         int yDirection = (mDestination.y - mOrigin.y) / absoluteDistance;
 
-        List<ChessCoordinate> spaces = new ArrayList();
+        List<ChessCoordinate> spaces = new ArrayList<>();
         for (int index = 1; index <= absoluteDistance && index <= maxSteps; index++) {
             spaces.add(ChessCoordinate.at(mOrigin.x + index * xDirection, mOrigin.y + index * yDirection));
         }
