e92febc7f1b515e68dcabe2a18d71fe341f99341
Drew Hannay <drewhannay@gmail.com>
2015-01-18 15:46:46 -0800

    Maintain Move history in a Stack

--- ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/controllers/GameController.java
+++ ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/controllers/GameController.java
@@ -1,10 +1,11 @@
 package com.drewhannay.chesscrafter.controllers;
 
-import com.drewhannay.chesscrafter.models.*;
+import com.drewhannay.chesscrafter.models.Board;
+import com.drewhannay.chesscrafter.models.ChessCoordinate;
+import com.drewhannay.chesscrafter.models.Game;
+import com.drewhannay.chesscrafter.models.Move;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
 
-import java.util.List;
 import java.util.Set;
 
 public final class GameController {
@@ -13,12 +14,6 @@ public final class GameController {
 
     public static void setGame(Game game) {
         sGame = game;
-        //mDataMap = Maps.newConcurrentMap();
-        computeLegalDestinations();
-    }
-
-    private static void verifyGameIsSet() {
-        Preconditions.checkArgument(sGame != null);
     }
 
     public static Game getGame() {
@@ -27,171 +22,17 @@ public final class GameController {
         return sGame;
     }
 
+    private static void verifyGameIsSet() {
+        Preconditions.checkArgument(sGame != null);
+    }
+
     public static Set<ChessCoordinate> getLegalDestinations(Board board, ChessCoordinate coordinate) {
-        //computeLegalDestinations();
-        //int teamId = piece.getTeamId(sGame);
-        //return mDataMap.get(teamId).getLegalDests(piece);
-        mLegalDestinations = board.getMovesFrom(coordinate);
-        return mLegalDestinations;
+        return board.getMovesFrom(coordinate);
     }
 
-
-    public static void computeLegalDestinations() {
-        // Piece[] threats = null;
-        // Piece movingObjectivePiece = null;
-        // Piece otherObjectivePiece = null;
-        // List<Piece> movingTeam = null;
-        // List<Piece> otherTeam = null;
-        /*
-        Team[] teams = sGame.getTeams();
-
-        for (int teamIndex = 0; teamIndex < teams.length; teamIndex++) {
-            mDataMap.put(teamIndex, new ComputedPieceData(teamIndex));
-            for (Piece piece : teams[teamIndex].getPieces()) {
-                int boardIndex =
-                        teams[teamIndex].getRules() == null ? piece.getCoordinates().boardIndex : teams[teamIndex].getRules().getDestinationBoardIndex(
-                                piece.getCoordinates().boardIndex);
-                mDataMap.get(teamIndex).computeLegalDestinations(piece, boardIndex);
-            }
-
-        }
-        /*
-        TODO: deal with all this stuff
-        movingObjectivePiece = (isBlackMove()) ? mBlackRules.objectivePiece(true) : mWhiteRules.objectivePiece(false);
-        movingTeam = (isBlackMove()) ? getBlackTeam() : getWhiteTeam();
-        otherObjectivePiece = (isBlackMove()) ? mBlackRules.objectivePiece(true) : mWhiteRules.objectivePiece(false);
-        otherTeam = (isBlackMove()) ? getWhiteTeam() : getBlackTeam();
-        // Make sure the objective piece doesn't put himself in check
-        if (movingObjectivePiece != null)
-        (movingObjectivePiece.isBlack() ? getBlackRules() : getWhiteRules()).cropLegalDests(movingObjectivePiece, movingObjectivePiece, movingTeam);
-        if (otherObjectivePiece != null)
-        (otherObjectivePiece.isBlack() ? getBlackRules() : getWhiteRules()).cropLegalDests(otherObjectivePiece, otherObjectivePiece, otherTeam);
-        if (movingObjectivePiece != null)
-        {
-        // Now see if any of the moves puts the objective piece in check and
-        // are therefore illegal
-        for (int i = 0; i < otherTeam.size(); i++)
-        {
-        if (otherTeam.equals(getWhiteTeam()))
-        getWhiteRules().cropLegalDests(movingObjectivePiece, otherTeam.get(i), movingTeam);
-        else
-        getBlackRules().cropLegalDests(movingObjectivePiece, otherTeam.get(i), movingTeam);
-        }
-        }
-        (isBlackMove() ? getBlackRules() : getWhiteRules()).adjustTeamLegalDestinations(movingTeam);
-        // if the objective piece is in check, the legal moves list must be
-        // modified accordingly
-        if (movingObjectivePiece != null && movingObjectivePiece.isInCheck())
-        {
-        if (getLastMove() != null)
-        getLastMove().setCheck(true);
-        threats = getThreats(movingObjectivePiece);
-        switch (threats.length)
-        {
-        case 1:
-        // there is only one threat, so another Piece could block, or
-        // the King could move
-        for (int i = 0; i < movingTeam.size(); i++)
-        movingTeam.get(i).computeLegalDestsToSaveObjective(movingObjectivePiece, threats[0]);
-        break;
-        case 2:
-        // since there is two threats, the objective piece is the only
-        // one who can get himself out of check.
-        for (int i = 0; i < movingTeam.size(); i++)
-        {
-        ComputedPieceData p = (movingTeam.get(i));
-        if (p != movingObjectivePiece)
-        {
-        p.getLegalDests().clear();
-        p.getGuardSquares().clear();
-        }
-        }
-        if (getLastMove() != null)
-        getLastMove().setDoubleCheck(true);
-        break;
-        }
-        }
-        */
-    }
-
-    /**
-     * Get the Pieces of the specified team guarding the specified Square
-     *
-     * @param coordinates    The Square being guarded
-     * @param guardTeamIndex The team guarding the Square
-     * @return The Pieces guarding the Square
-     */
-    public static List<Piece> getGuards(ChessCoordinate coordinates, int guardTeamIndex) {
-//        List<Piece> pieces = sGame.getTeams()[guardTeamIndex].getPieces();
-        List<Piece> guards = Lists.newArrayList();
-
-        /*for (Piece piece : pieces) {
-            // TODO: use updated methods
-            if (mDataMap.get(guardTeamIndex).isGuarding(piece, coordinates))
-                guards.add(piece);
-        }*/
-
-        return guards;
-    }
-
-    /**
-     * Get a count of all legal moves for this turn.
-     *
-     * @return The number of legal moves this turn.
-     */
-    public static int getLegalMoveCount() {
-        //int teamIndex = sGame.getTurnKeeper().getActiveTeamId();
-        // List<Piece> movingTeam = sGame.getTeams()[teamIndex].getPieces();
-
-        //int count = 0;
-        //for (Piece piece : movingTeam)
-        //    count += mDataMap.get(teamIndex).getLegalDests(piece).size();
-
-        //return count;
-
-        return mLegalDestinations.size();
-    }
-
-    private static List<Piece> getThreats(ChessCoordinate threatened, int attackerTeamIndex) {
-//        List<Piece> pieces = sGame.getTeams()[attackerTeamIndex].getPieces();
-        List<Piece> attackers = Lists.newArrayList();
-
-        /*for (Piece piece : pieces) {
-            @TODO Use updated methods
-            if (mDataMap.get(attackerTeamIndex).canLegallyAttack(piece, threatened))
-                attackers.add(piece);
-        }*/
-
-        return attackers;
-    }
-
-    public static boolean isGuarded(ChessCoordinate coordinates, int teamIndex) {
-        return getGuards(coordinates, teamIndex) != null;
-    }
-
-    public static boolean isThreatened(ChessCoordinate coordinates, int teamIndex) {
-        return getThreats(coordinates, teamIndex) != null;
-    }
-
-    public static boolean isLegalMove(Piece piece, ChessCoordinate destination) {
-        //int teamIndex = sGame.getTurnKeeper().getActiveTeamId();
-
-        //return mDataMap.get(teamIndex).getLegalDests(piece).contains(destination);
-        return mLegalDestinations.contains(destination);
-    }
-
-    /**
-     * Play a move in the Game
-     *
-     * @param move The Move to play
-     * @throws Exception If the Move was illegal
-     */
     public static void playMove(Move move) {
         sGame.executeMove(move);
     }
 
     private static Game sGame;
-    //private static Map<Integer, ComputedPieceData> mDataMap;
-    private static Set<ChessCoordinate> mLegalDestinations;
-
 }
--- ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/models/Game.java
+++ ChessCrafterShared/src/main/java/com/drewhannay/chesscrafter/models/Game.java
@@ -4,18 +4,22 @@ import com.drewhannay.chesscrafter.models.turnkeeper.TurnKeeper;
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Stack;
+
 public final class Game {
 
     private final String mGameType;
     private final Team[] mTeams;
     private final Board[] mBoards;
     private final TurnKeeper mTurnKeeper;
+    private final Stack<Move> mHistory;
 
     public Game(String gameType, Board[] boards, Team[] teams, TurnKeeper turnKeeper) {
         mGameType = gameType;
         mBoards = boards;
         mTeams = teams;
         mTurnKeeper = turnKeeper;
+        mHistory = new Stack<>();
     }
 
     public String getGameType() {
@@ -58,9 +62,15 @@ public final class Game {
         // TODO: Check end condition
 
         mTurnKeeper.finishTurn();
+
+        mHistory.push(move);
     }
 
-    public void undoMove(@NotNull Move move) {
+    public void undoMove() {
+        Preconditions.checkState(!mHistory.isEmpty());
+
+        Move move = mHistory.pop();
+
         mTurnKeeper.undoFinishTurn();
 
         Board board = mBoards[0];
