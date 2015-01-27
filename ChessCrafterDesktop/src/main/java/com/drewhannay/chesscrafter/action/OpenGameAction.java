b51f19cfa57ab49d99831e5e8367dd5d549e6c37
Drew Hannay <drewhannay@gmail.com>
2015-01-27 10:36:41 -0800

    Open saved games into a new tab instead of a new window

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/action/OpenGameAction.java
+++ ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/action/OpenGameAction.java
@@ -1,26 +1,19 @@
 package com.drewhannay.chesscrafter.action;
 
-import com.drewhannay.chesscrafter.frame.Driver;
 import com.drewhannay.chesscrafter.utility.Messages;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 public final class OpenGameAction extends ChessAction {
     public OpenGameAction() {
-        super(false);
+        super(true);
 
         putValue(NAME, Messages.getString("Driver.openGame"));
         putValue(SHORT_DESCRIPTION, Messages.getString("Driver.openASavedGame"));
 
         putValue(MNEMONIC_KEY, KeyEvent.VK_O);
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        Driver.openGame();
     }
 }
