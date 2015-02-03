1fb941c9954008ab67091e6a8062ee6f4960b7b7
Drew Hannay <drewhannay@gmail.com>
2015-02-03 13:22:27 -0800

    Toggle Mac fullscreen mode with Cmd+Shift+F in any ChessFrame

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/frame/ChessFrame.java
+++ ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/frame/ChessFrame.java
@@ -1,5 +1,6 @@
 package com.drewhannay.chesscrafter.frame;
 
+import com.apple.eawt.Application;
 import com.drewhannay.chesscrafter.action.ChessActions;
 import com.drewhannay.chesscrafter.dragNdrop.GlassPane;
 import com.drewhannay.chesscrafter.utility.FileUtility;
@@ -7,6 +8,8 @@ import com.drewhannay.chesscrafter.utility.Log;
 import com.google.common.base.Preconditions;
 
 import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.KeyStroke;
@@ -15,6 +18,7 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import java.lang.reflect.Method;
@@ -52,12 +56,24 @@ class ChessFrame extends JFrame {
         setJMenuBar(ChessActions.createJMenuBar());
         setPreferredSize(new Dimension(685, 450));
 
-        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
-                .put(KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), "close");
-        getRootPane().getActionMap().put("close", new AbstractAction() {
+        InputMap inputMap = getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), "close");
+        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.SHIFT_DOWN_MASK | Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), "fullscreen");
+
+        ActionMap actionMap = getRootPane().getActionMap();
+        actionMap.put("close", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 onWindowCloseKeyboardRequest();
+            }
+        });
+        actionMap.put("fullscreen", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application application = Application.getApplication();
+                if (application != null) {
+                    application.requestToggleFullScreen(ChessFrame.this);
+                }
             }
         });
 
