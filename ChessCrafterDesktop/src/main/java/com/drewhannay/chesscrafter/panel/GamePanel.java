20e4853fa9475edd7766e1a666c0b06adb1ed1b7
jmccormi <johnnymac32@gmail.com>
2015-02-03 09:44:34 -0600

    Fixing imports

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/panel/GamePanel.java
+++ ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/panel/GamePanel.java
@@ -20,7 +20,12 @@ import com.drewhannay.chesscrafter.utility.PieceIconUtility;
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
