29cf71c58db8f5c793330e44bf02543efec61f40
Drew Hannay <drewhannay@gmail.com>
2015-02-03 13:07:03 -0800

    Fix string formatting in About frame

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/action/AboutAction.java
+++ ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/action/AboutAction.java
@@ -1,6 +1,5 @@
 package com.drewhannay.chesscrafter.action;
 
-import com.drewhannay.chesscrafter.frame.AboutFrame;
 import com.drewhannay.chesscrafter.frame.FrameManager;
 import com.drewhannay.chesscrafter.utility.AppConstants;
 import com.drewhannay.chesscrafter.utility.Messages;
@@ -12,9 +11,8 @@ public class AboutAction extends ChessAction {
     AboutAction() {
         super(false);
 
-        // TODO: should insert app name into formatted string
-        putValue(NAME, Messages.getString("Driver.about") + AppConstants.APP_NAME);
-        putValue(SHORT_DESCRIPTION, Messages.getString("Driver.informationAbout") + AppConstants.APP_NAME);
+        putValue(NAME, Messages.getString("Driver.about", AppConstants.APP_NAME));
+        putValue(SHORT_DESCRIPTION, Messages.getString("Driver.informationAbout", AppConstants.APP_NAME));
 
         putValue(MNEMONIC_KEY, KeyEvent.VK_A);
     }
