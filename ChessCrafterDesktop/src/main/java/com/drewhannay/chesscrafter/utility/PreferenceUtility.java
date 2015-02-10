4d6a2ec4da791fa03a96e42e64d2059b6a3080ac
jmccormi <johnnymac32@gmail.com>
2015-02-10 09:56:44 -0600

    Removing Save location from preferences.
    Turned preferences into ChessDialog.

--- ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/utility/PreferenceUtility.java
+++ ChessCrafterDesktop/src/main/java/com/drewhannay/chesscrafter/utility/PreferenceUtility.java
@@ -1,17 +1,12 @@
 package com.drewhannay.chesscrafter.utility;
 
 import com.drewhannay.chesscrafter.Main;
+import com.drewhannay.chesscrafter.dialog.ChessDialog;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -27,75 +22,36 @@ public final class PreferenceUtility {
     }
 
     public static void createPreferencePopup(Component relativeComponent) {
-        JFrame popupFrame = new JFrame(Messages.getString("PreferenceUtility.preferences"));
-        popupFrame.setSize(370, 120);
-        popupFrame.setLocationRelativeTo(relativeComponent);
-        popupFrame.setLayout(new GridBagLayout());
-        popupFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        ChessDialog preferenceDialog = new ChessDialog(true);
+        preferenceDialog.setSize(250, 120);
+        preferenceDialog.setLocationRelativeTo(relativeComponent);
+        preferenceDialog.setLayout(new GridBagLayout());
+        preferenceDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        preferenceDialog.setTitle(Messages.getString("PreferenceUtility.preferences"));
         GridBagConstraints constraints = new GridBagConstraints();
 
-        String userSaveLocation = "Delete Me!";
-
-        JPanel holder = new JPanel();
-        holder.setBorder(BorderFactory.createTitledBorder(Messages.getString("PreferenceUtility.defaultCompletedLocation")));
-        JLabel currentSaveLocationLabel = new JLabel(Messages.getString("PreferenceUtility.currentSaveLocation"));
-        JTextField currentSaveLocationField = new JTextField("Delete Me!");
-        currentSaveLocationField.setEditable(false);
-        int width = Math.max(userSaveLocation.length() + 15, 300);
-        currentSaveLocationField.setPreferredSize(new Dimension(width, 25));
-        JButton changeLocationButton = new JButton(Messages.getString("PreferenceUtility.chooseNewSaveLocation"));
-        JButton resetButton = new JButton(Messages.getString("PreferenceUtility.resetToDefaultLocation"));
         JCheckBox highlightingCheckBox = new JCheckBox(Messages.getString("PreferenceUtility.enableHighlighting"));
 
         JButton closeButton = new JButton(Messages.getString("PreferenceUtility.close"));
-        closeButton.addActionListener(event1 -> popupFrame.dispose());
+        closeButton.addActionListener(event1 -> preferenceDialog.dispose());
 
-        holder.add(currentSaveLocationLabel);
-        holder.add(currentSaveLocationField);
-
-        currentSaveLocationField.setText(userSaveLocation);
         highlightingCheckBox.setSelected(getHighlightMovesPreference());
-
-        resetButton.addActionListener(event -> {
-            currentSaveLocationField.setText("Delete Me");
-        });
-
-        changeLocationButton.addActionListener(event -> {
-            // TODO: delete me
-        });
-
         highlightingCheckBox.addActionListener(event -> setHighlightMovesPreference(highlightingCheckBox.isSelected()));
 
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 2;
         constraints.insets = new Insets(5, 5, 5, 5);
-        popupFrame.add(holder, constraints);
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.weightx = 1;
-        constraints.gridwidth = 1;
-        constraints.anchor = GridBagConstraints.EAST;
-        popupFrame.add(changeLocationButton, constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.anchor = GridBagConstraints.WEST;
-        popupFrame.add(resetButton, constraints);
-
         constraints.gridx = 0;
         constraints.gridy = 3;
         constraints.gridwidth = 1;
         constraints.anchor = GridBagConstraints.CENTER;
-        popupFrame.add(highlightingCheckBox, constraints);
+        preferenceDialog.add(highlightingCheckBox, constraints);
 
         constraints.gridx = 0;
         constraints.gridy = 4;
         constraints.gridwidth = 2;
         constraints.anchor = GridBagConstraints.CENTER;
-        popupFrame.add(closeButton, constraints);
+        preferenceDialog.add(closeButton, constraints);
 
-        popupFrame.pack();
-        popupFrame.setVisible(true);
+        preferenceDialog.setVisible(true);
     }
 
     /**
